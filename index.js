50a388fb4c638c44d8453c46a19488c1b1958cfb
melissa sidisaid <milyssasidisaid@gmail.com>
2023-07-20 12:39:36 +0200

    done

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
-// Write your code here
+
+
+let num1=62;
+let num2=1;
+let multiply=num2*num1;
+let random = +1;
+console.log(Math.random);
+let max =Math.max(0, 15, 3, 20, 8);
+let num3=1;
+let num4=4;
+let mod=num4/num3;
